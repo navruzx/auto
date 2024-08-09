e2de382743ba6ef8a0f816e9c23138b427b6b8ec
navruzx <nnavruz095@gmail.com>
2024-08-09 17:57:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+// // // // class Kasb{
+// // // //     constructor(name,familiya,age,project_number) {
+// // // //         this.name = name;
+// // // //         this.familiya = familiya;
+// // // //         this.age = age;
+// // // //         this.project_number = project_number;
+// // // //     }
+// // // // }
+// // // // class Programmer extends Kasb {
+// // // //     constructor(name,familiya,age,project_number) {
+// // // //         super(name,familiya,age);
+// // // //         this.project_number = project_number;
+// // // //     }
+// // // // }
+// // // // let oka = new  Kasb("Aziz","Azizov",22,22);
+// // // // console.log(oka);
+// // // class user{
+// // //     constructor(name,nicname,familiyasi,yoshi){
+// // //         this.name = name;
+// // //         this.familiyasi = familiyasi;
+// // //         this.nicname = nicname;
+// // //         this.yoshi = yoshi;
+// // //     }
+// // //     get_info()
+// // //     {console.log(`Foydalanuchi ismi ${this.name} foydalanuvchi familiyasi ${this.familiyasi} nicname ${this.nicname} yoshi ${this.yoshi}`)
+    
+// // //      }  
+// // // }
+// // // let user1 = new user("Ali","aliyevss","example@gmail.com",12)
+// // // user1.get_info();
+// // class auto{
+// //     constructor(model,rang,korobka,narx){
+// //         this.model = model;
+// //         this.rang = rang;
+// //         this.korobka = korobka;
+// //         this.narx = narx;
+// //     }
+// //      get_info()
+// //    {console.log(`Moshinani modeli ${this.model} moshina rangi ${this.rang} moshina korobkasi ${this.korobka} moshinani narxi ${this.narx}`)
+// // } 
+// // }
+// // let auto1 = new auto("XM","black","avtomat",200.000)
+// // auto1.get_info();
+// class autosalon{
+//     constructor(name,adress,auto){
+//         this.name = name;
+//         this.adress = adress;
+//         this.auto = auto;
+//     }
+//     get_info(){
+//         {console.log(`Autosalon ismi ${this.name} autosalon addresi ${this.adress} autosalondagi moshinalar ${this.auto}`)     
+//     }
+// }
+// }
+// let autosalon1 = new autosalon("navruzcars","samarqand shahar vogzal","BMW,Mersedes benz,Porsche")
+// autosalon1.get_info();
+// let autosalon2 = new autosalon("navruzcars","toshkent shahri yunusobod tumani","istagan markali avtomashinangiz bor")
+// autosalon2.get_info();
+
+
